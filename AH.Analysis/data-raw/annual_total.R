ab165fe3bdd5334df72c8b033b21275ae2c21875
Yujie Xu <shenqing1225@gmail.com>
2023-01-05 12:01:51 -0800

    add script to compile simulation result by idf epw month

--- AH.Analysis/data-raw/annual_total.R
+++ AH.Analysis/data-raw/annual_total.R
@@ -1,4 +1,5 @@
 ## code compiling annual total goes in here
+## assume home dir is "AH.Analysis/data-raw"
 
 library("dplyr")
 
@@ -9,6 +10,10 @@ get.total <- function(df) {
         dplyr::ungroup() %>%
         {.}
 }
+
+## --------------------------------------------------------------
+## annual total by grid
+## --------------------------------------------------------------
 
 df.coarse = readr::read_csv("hourly_heat_energy/annual_2018.csv")
 
@@ -45,3 +50,69 @@ df.tract.ann <- df.tract %>%
 df.tract.ann
 
 usethis::use_data(df.tract.ann)
+
+
+## --------------------------------------------------------------
+## annual total by idf and epw
+## --------------------------------------------------------------
+
+## read simulation results for annual
+result.csv.dir = "EP_output_csv/sim_result_ann_WRF_2018_csv"
+files = list.files(result.csv.dir, pattern = "*.csv")
+
+head(files)
+
+result.mon <- lapply(files, function(f) {
+    print(f)
+    tokens = unlist(stringr::str_split(f, pattern = "____"))
+    idf.kw = tokens[[1]]
+    epw.id = gsub(".csv", "", tokens[[2]])
+    df = readr::read_csv(sprintf("%s/%s", result.csv.dir, f), col_types = readr::cols()) %>%
+        dplyr::mutate(emission.exfiltration = `Environment:Site Total Zone Exfiltration Heat Loss [J](Hourly)`,
+                      emission.exhaust = `Environment:Site Total Zone Exhaust Air Heat Loss [J](Hourly)`,
+                      emission.ref = `SimHVAC:Air System Relief Air Total Heat Loss Energy [J](Hourly)`,
+                      emission.rej = `SimHVAC:HVAC System Total Heat Rejection Energy [J](Hourly)`,
+                      emission.surf = `Environment:Site Total Surface Heat Emission to Air [J](Hourly)`,
+                      emission.overall = emission.exfiltration + emission.exhaust + emission.ref + emission.rej + emission.surf) %>%
+        dplyr::mutate(energy.elec = `Electricity:Facility [J](Hourly)`) %>%
+        dplyr::mutate(energy.overall = energy.elec) %>%
+        dplyr::mutate(idf.kw = idf.kw, epw.id = epw.id) %>%
+        {.}
+    if ("NaturalGas:Facility [J](Hourly)" %in% names(df)) {
+      df <- df %>%
+          dplyr::mutate(energy.gas = `NaturalGas:Facility [J](Hourly)`) %>%
+          dplyr::mutate(energy.overall = energy.elec + energy.gas)
+    }
+    if (nrow(df) != 8760) {
+        print(sprintf("%s: %d", f, nrow(df)))
+    }
+    df <- df %>%
+        dplyr::select(`Date/Time`, idf.kw, epw.id, starts_with("emission."), starts_with("energy")) %>%
+        tidyr::separate(`Date/Time`, into = c("month", "suffix"), sep = "/") %>%
+        dplyr::select(-suffix) %>%
+        dplyr::group_by(idf.kw, epw.id, month) %>%
+        dplyr::summarise_if(is.numeric, sum) %>%
+        dplyr::ungroup() %>%
+        {.}
+    df
+}) %>%
+  dplyr::bind_rows()
+
+## monthly simulation result
+mon.sim.result.by.idf.epw <- result.mon
+
+usethis::use_data(mon.sim.result.by.idf.epw)
+
+result.ann <- result.mon %>%
+    dplyr::group_by(idf.kw, epw.id) %>%
+    dplyr::summarise_if(is.numeric, sum) %>%
+    dplyr::ungroup() %>%
+    {.}
+
+ann.sim.result.by.idf.epw <- result.ann
+
+usethis::use_data(ann.sim.result.by.idf.epw)
+
+idf.kw.to.usetype <- readr::read_csv("idf_kw_to_EnergyAtlas_usetype.csv")
+
+usethis::use_data(idf.kw.to.usetype)
